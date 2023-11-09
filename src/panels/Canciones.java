9cfad0ee2096958457ff5f15ca49bf0d575afdef
xrev4n <m.osorio.d37@gmail.com>
2023-11-09 17:14:05 -0300

    Modifica Diseño Panel

---
+++ src/panels/Canciones.java
@@ -0,0 +1,19 @@
+package panels;
+
+
+import java.util.ArrayList;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author felip
+ */
+public class Canciones {
+    
+    ArrayList<Cancion> listaCanciones = new ArrayList();
+    
+}
